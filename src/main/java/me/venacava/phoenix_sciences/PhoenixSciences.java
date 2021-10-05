5c3a67518c4facae0369e2a346a5925bb204bda6
Vena <jakemansfield66@gmail.com>
2021-10-05 21:37:40 +0100

    Updates

--- src/main/java/me/venacava/phoenix_sciences/PhoenixSciences.java
+++ src/main/java/me/venacava/phoenix_sciences/PhoenixSciences.java
@@ -1,9 +1,12 @@
 package me.venacava.phoenix_sciences;
 
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
+import org.bukkit.Effect;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
@@ -15,6 +18,8 @@ import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.skins.PlayerHead;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.skins.PlayerSkin;
+import org.bukkit.potion.PotionData;
+import org.bukkit.potion.PotionType;
 
 public class PhoenixSciences extends JavaPlugin implements SlimefunAddon {
 
@@ -41,7 +46,7 @@ public class PhoenixSciences extends JavaPlugin implements SlimefunAddon {
         /*
          * Synthetic Ruby
          */
-        SlimefunItemStack SyntheticRuby = new SlimefunItemStack("SYNTHETIC_RUBY", PlayerHead.getItemStack(PlayerSkin.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzIwZWYwNmRkNjA0OTk3NjZhYzhjZTE1ZDJiZWE0MWQyODEzZmU1NTcxODg2NGI1MmRjNDFjYmFhZTFlYTkxMyJ9fX0=")), "&4Synthetic Ruby", "&c+20% Coolness");
+        SlimefunItemStack SyntheticRuby = new SlimefunItemStack("PHOENIXSCIENCES_SYNTHETIC_RUBY", PlayerHead.getItemStack(PlayerSkin.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzIwZWYwNmRkNjA0OTk3NjZhYzhjZTE1ZDJiZWE0MWQyODEzZmU1NTcxODg2NGI1MmRjNDFjYmFhZTFlYTkxMyJ9fX0=")), "&4Synthetic Ruby", "&c+20% Coolness");
 
 
         ItemStack[] SyntheticRubyRecipe = { SlimefunItems.ALUMINUM_DUST,  new ItemStack(Material.GLASS),    new ItemStack(Material.GLASS_PANE),
@@ -54,7 +59,7 @@ public class PhoenixSciences extends JavaPlugin implements SlimefunAddon {
         /*
          * Synthetic Topaz
          */
-        SlimefunItemStack SyntheticTopaz = new SlimefunItemStack("SYNTHETIC_TOPAZ", PlayerHead.getItemStack(PlayerSkin.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMmM3NjY3ZGJlMjZmNjA3YjY5OWM5MzAzZTc0Zjg5NTEwZGFjNzU0M2UwYzkyY2VmOTliMzM0NTQ5YzU3YzQ4YSJ9fX0=")), "&eSynthetic Topaz", "&c+20% Coolness");
+        SlimefunItemStack SyntheticTopaz = new SlimefunItemStack("PHOENIXSCIENCES_SYNTHETIC_TOPAZ", PlayerHead.getItemStack(PlayerSkin.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMmM3NjY3ZGJlMjZmNjA3YjY5OWM5MzAzZTc0Zjg5NTEwZGFjNzU0M2UwYzkyY2VmOTliMzM0NTQ5YzU3YzQ4YSJ9fX0=")), "&eSynthetic Topaz", "&c+20% Coolness");
 
         ItemStack[] SyntheticTopazRecipe = { SlimefunItems.ALUMINUM_DUST,  new ItemStack(Material.GLASS),         new ItemStack(Material.GLASS_PANE),
                                              SlimefunItems.ALUMINUM_INGOT, new ItemStack(Material.YELLOW_DYE),    null,
@@ -66,7 +71,7 @@ public class PhoenixSciences extends JavaPlugin implements SlimefunAddon {
         /*
          * Synthetic Amethyst
          */
-        SlimefunItemStack SyntheticAmethyst = new SlimefunItemStack("SYNTHETIC_AMETHYST", PlayerHead.getItemStack(PlayerSkin.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvY2FkOGNjOTgyNzg2ZmI0ZDQwYjBiNmU2NGE0MWYwZDk3MzZmOWMyNmFmZmI4OThmNGE3ZmFlYTg4Y2NmODk5NyJ9fX0=")), "&dSynthetic Amethyst", "&c+20% Coolness");
+        SlimefunItemStack SyntheticAmethyst = new SlimefunItemStack("PHOENIXSCIENCES_SYNTHETIC_AMETHYST", PlayerHead.getItemStack(PlayerSkin.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvY2FkOGNjOTgyNzg2ZmI0ZDQwYjBiNmU2NGE0MWYwZDk3MzZmOWMyNmFmZmI4OThmNGE3ZmFlYTg4Y2NmODk5NyJ9fX0=")), "&dSynthetic Amethyst", "&c+20% Coolness");
 
         ItemStack[] SyntheticAmethystRecipe = { SlimefunItems.ALUMINUM_DUST,  new ItemStack(Material.GLASS),         new ItemStack(Material.GLASS_PANE),
                                                 SlimefunItems.ALUMINUM_INGOT, SlimefunItems.SYNTHETIC_SAPPHIRE,      SynthRub.getItem(),
@@ -78,7 +83,7 @@ public class PhoenixSciences extends JavaPlugin implements SlimefunAddon {
         /*
          * Synthetic Amber
          */
-        SlimefunItemStack SyntheticAmber = new SlimefunItemStack("SYNTHETIC_AMBER", PlayerHead.getItemStack(PlayerSkin.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYWFjZTZiYjNhYTRjY2FjMDMxMTY4MjAyZjZkNDUzMjU5N2JjYWM2MzUxMDU5YWJkOWQxMGIyODYxMDQ5M2FlYiJ9fX0=")), "&6Synthetic Amber", "&c+20% Coolness");
+        SlimefunItemStack SyntheticAmber = new SlimefunItemStack("PHOENIXSCIENCES_SYNTHETIC_AMBER", PlayerHead.getItemStack(PlayerSkin.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYWFjZTZiYjNhYTRjY2FjMDMxMTY4MjAyZjZkNDUzMjU5N2JjYWM2MzUxMDU5YWJkOWQxMGIyODYxMDQ5M2FlYiJ9fX0=")), "&6Synthetic Amber", "&c+20% Coolness");
 
         ItemStack[] SyntheticAmberRecipe = { SlimefunItems.ALUMINUM_DUST,  new ItemStack(Material.GLASS),         new ItemStack(Material.GLASS_PANE),
                                              SlimefunItems.ALUMINUM_INGOT, SynthTop.getItem(),                    SynthRub.getItem(),
@@ -90,7 +95,7 @@ public class PhoenixSciences extends JavaPlugin implements SlimefunAddon {
         /*
          * Synthetic Quartz
          */
-        SlimefunItemStack SyntheticQuartz = new SlimefunItemStack("SYNTHETIC_QUARTZ", PlayerHead.getItemStack(PlayerSkin.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZThjZGU2NzAwMzcyMmRiODE4Mjk5YmNiZWM4ODMyODlkMThkYzkzMzJkZTgwOTgzNDAwYTg3ZjIwNTQ0MzkyYSJ9fX0=")), "&fSynthetic Quartz", "&c+20% Coolness");
+        SlimefunItemStack SyntheticQuartz = new SlimefunItemStack("PHOENIXSCIENCES_SYNTHETIC_QUARTZ", PlayerHead.getItemStack(PlayerSkin.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZThjZGU2NzAwMzcyMmRiODE4Mjk5YmNiZWM4ODMyODlkMThkYzkzMzJkZTgwOTgzNDAwYTg3ZjIwNTQ0MzkyYSJ9fX0=")), "&fSynthetic Quartz", "&c+20% Coolness");
 
         ItemStack[] SyntheticQuartzRecipe = { SlimefunItems.ALUMINUM_DUST,  new ItemStack(Material.GLASS),         new ItemStack(Material.GLASS_PANE),
                                               SlimefunItems.ALUMINUM_INGOT, new ItemStack(Material.BONE_MEAL),     null,
@@ -102,7 +107,7 @@ public class PhoenixSciences extends JavaPlugin implements SlimefunAddon {
         /*
          * Synthetic Onyx
          */
-        SlimefunItemStack SyntheticOnyx = new SlimefunItemStack("SYNTHETIC_ONYX", PlayerHead.getItemStack(PlayerSkin.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzY5ZWM3MjJkOWNiNmVkOWJhZTkzYzI2NmJkMDk4ZjBhMzA2YzVlOGMwMzg0OWMyN2NkY2YxOGM2MGQ2Y2IzZSJ9fX0=")), "&0Synthetic Onyx", "&c+20% Coolness");
+        SlimefunItemStack SyntheticOnyx = new SlimefunItemStack("PHOENIXSCIENCES_SYNTHETIC_ONYX", PlayerHead.getItemStack(PlayerSkin.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzY5ZWM3MjJkOWNiNmVkOWJhZTkzYzI2NmJkMDk4ZjBhMzA2YzVlOGMwMzg0OWMyN2NkY2YxOGM2MGQ2Y2IzZSJ9fX0=")), "&0Synthetic Onyx", "&c+20% Coolness");
 
         ItemStack[] SyntheticOnyxRecipe = { SlimefunItems.ALUMINUM_DUST,  new ItemStack(Material.GLASS),         new ItemStack(Material.GLASS_PANE),
                                             SlimefunItems.ALUMINUM_INGOT, new ItemStack(Material.BLACK_DYE),     null,
@@ -114,7 +119,7 @@ public class PhoenixSciences extends JavaPlugin implements SlimefunAddon {
         /*
          * Synthetic Paragon
          */
-        SlimefunItemStack SyntheticParagon = new SlimefunItemStack("SYNTHETIC_PARAGON", PlayerHead.getItemStack(PlayerSkin.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDlhZTg4YTdkMDNmNDc0NTU4YTA1NjkyZTVmNWMzYWRlMzEyZGRmMTA3MjE2NmFkMDQyNjMzNGVmNTE3NGI4NyJ9fX0=")), "&f&lSynthetic Paragon", "&fThe Perfect Gemstone. 100% Purity.");
+        SlimefunItemStack SyntheticParagon = new SlimefunItemStack("PHOENIXSCIENCES_SYNTHETIC_PARAGON", PlayerHead.getItemStack(PlayerSkin.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDlhZTg4YTdkMDNmNDc0NTU4YTA1NjkyZTVmNWMzYWRlMzEyZGRmMTA3MjE2NmFkMDQyNjMzNGVmNTE3NGI4NyJ9fX0=")), "&f&lSynthetic Paragon", "&fThe Perfect Gemstone. 100% Purity.");
 
         ItemStack[] SyntheticParagonRecipe = { SynthAme.getItem(),  SlimefunItems.SYNTHETIC_SAPPHIRE,   SlimefunItems.SYNTHETIC_EMERALD,
                                                SynthQua.getItem(),  SlimefunItems.SYNTHETIC_DIAMOND,    SynthOny.getItem(),
@@ -126,22 +131,27 @@ public class PhoenixSciences extends JavaPlugin implements SlimefunAddon {
         /*
          * Liquid Air
          */
-        SlimefunItemStack LiquidAir = new SlimefunItemStack("LIQUID_AIR", Material.WATER_BOTTLE), "Liquid Air", "&7Supercold Air, so cold that it condensed into a liquid.");
+        SlimefunItemStack LiquidAir = new SlimefunItemStack("PHOENIXSCIENCES_LIQUID_AIR", Material.POTION, "&fLiquid Air", "&7Supercold Air, so cold that it condensed into a liquid.");
 
-        ItemStack[] LiquidAirRecipe = { new ItemStack(Material.GLASS_BOTTLE),   null,              null };
-                                        null,                                   null,              null };
+        PotionMeta meta = (PotionMeta) LiquidAir.getItemMeta();
+                meta.setBasePotionData(new PotionData(PotionType.WATER));
+                meta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
+                LiquidAir.setItemMeta(meta);
+
+        ItemStack[] LiquidAirRecipe = { new ItemStack(Material.GLASS_BOTTLE),   null,              null,
+                                        null,                                   null,              null,
                                         null,                                   null,              null };
 
-        SlimefunItem LiquiAir = new SlimefunItem(itemGroup, LiquidAir, RecipeType.FREEZER, LiquidAirRecipe);
-        LiquiAir.register(this);
+        SlimefunItem LiqAir = new SlimefunItem(itemGroup, LiquidAir, RecipeType.FREEZER, LiquidAirRecipe);
+        LiqAir.register(this);
         
         /*
          * Neon Gas
          */
-        SlimefunItemStack NeonGas = new SlimefunItemStack("NEON_GAS", Material.DRAGON_BREATH), "Neon Gas", "&7Neon Gas in a Bottle.");
+        SlimefunItemStack NeonGas = new SlimefunItemStack("PHOENIXSCIENCES_NEON_GAS", Material.DRAGON_BREATH, "&fNeon Gas", "&7Neon Gas in a Bottle.");
 
-        ItemStack[] NeonGasRecipe = { LiquiAir.getItem(),       null,       null };
-                                      null,                     null,       null };
+        ItemStack[] NeonGasRecipe = { LiqAir.getItem(),       null,       null,
+                                      null,                     null,       null,
                                       null,                     null,       null };
 
         SlimefunItem NeoGas = new SlimefunItem(itemGroup, NeonGas, RecipeType.SMELTERY, NeonGasRecipe);
